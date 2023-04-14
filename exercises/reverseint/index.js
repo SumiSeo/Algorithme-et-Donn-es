0da011d1b517f3e26441885624e3188b062e5b77
Sumi <equepem@gmail.com>
2023-04-14 14:27:40 +0200

    feat(reverseInt): solve reverseint math func

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,10 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  const checked = Math.sign(n);
+  const num = parseInt(n.toString().split("").reverse().join(""));
+  return checked * num;
+}
 
 module.exports = reverseInt;
