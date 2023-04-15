27eb560c80427dd37656551bbaf057a6e2de873a
Sumi <equepem@gmail.com>
2023-04-15 14:34:36 +0200

    feat(maxchars): solve max char problems with object and entries

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,26 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  //loop over the string
+  const obj = {};
+  for (char of str) {
+    obj[char] = obj[char] + 1 || 1;
+  }
+
+  //and create object by counting each letter
+  const values = Object.values(obj);
+  const max = Math.max(...values);
+  let answer;
+
+  Object.entries(obj).forEach(([key, value]) => {
+    if (value === max) {
+      answer = key;
+    }
+  });
+
+  return answer;
+  //and find the max char
+}
 
 module.exports = maxChar;
