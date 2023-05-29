6d2071719376519f835f00838f0c5de4fa5b78ea
Sumi <equepem@gmail.com>
2023-05-29 15:46:06 +0200

    solve max char with two for loops

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,21 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const obj = {};
+  for (let char of str) {
+    obj[char] = obj[char] + 1 || 1;
+  }
+
+  let max = 0;
+  let maxChar = "";
+  for (let str in obj) {
+    if (obj[str] > max) {
+      max = obj[str];
+      maxChar = str;
+    }
+  }
+  return maxChar;
+}
 
 module.exports = maxChar;
