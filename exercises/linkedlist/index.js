b766bc28556e9558fee9c164ebfc2fb6198d8b2a
Sumi <equepem@gmail.com>
2023-06-13 17:16:05 +0200

    solve insertfirst and linkedlist

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,8 +2,33 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
 
-class LinkedList {}
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  insertFirst(data) {
+    const n = new Node(data, this.head);
+    this.head = n;
+  }
+
+  size() {
+    //loop over node until the next value is over
+    let counter = 0;
+    let node = this.head;
+    while (node) {
+      counter++;
+      node = node.next;
+    }
+    return counter;
+  }
+}
 
 module.exports = { Node, LinkedList };
